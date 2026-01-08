a41dad10fb0c37c53408d042deae0f50312362f2
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-01-08 16:30:32 +0530

    .

---
+++ sqrt_x.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
