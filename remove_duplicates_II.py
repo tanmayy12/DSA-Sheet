796ee523a34b4dcae9e58b478c67a999c536fe82
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-01-16 18:49:53 +0530

    .

---
+++ remove_duplicates_II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        n = len(nums)
+        if n <= 2:
+            return n
+
+        k = 2  # index for placing valid elements
+
+        for i in range(2, n):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
