37acf5d6a8f85bbd41556364de67ebf0c263af91
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-01-21 18:34:34 +0530

    .

---
+++ pascals_triangle_II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int):
+        row = [0] * (rowIndex + 1)
+        row[0] = 1
+        
+        for i in range(1, rowIndex + 1):
+            for j in range(i, 0, -1):
+                row[j] += row[j - 1]
+        
+        return row
