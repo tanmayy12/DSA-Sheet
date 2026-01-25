ffe30284a9b6847ebe28b28e233a050536f23174
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-01-25 20:19:51 +0530

    .

---
+++ isomorphic_strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        mapST = {}
+        mapTS = {}
+
+        for c1, c2 in zip(s, t):
+            if (c1 in mapST and mapST[c1] != c2) or \
+               (c2 in mapTS and mapTS[c2] != c1):
+                return False
+
+            mapST[c1] = c2
+            mapTS[c2] = c1
+
+        return True
