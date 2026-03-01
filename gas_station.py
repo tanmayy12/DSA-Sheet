eccc99e541d5cea4862d1822a79ce4063e03be45
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-03-01 17:55:24 +0530

    .

---
+++ gas_station.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        total_tank = 0
+        current_tank = 0
+        start = 0
+        
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            
+            total_tank += diff
+            current_tank += diff
+            
+            # If we cannot reach next station
+            if current_tank < 0:
+                start = i + 1
+                current_tank = 0
+        
+        if total_tank < 0:
+            return -1
+        return start
