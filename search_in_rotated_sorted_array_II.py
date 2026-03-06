ad2516b5525a5960fc64490718b2e7a98cc4a76c
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-03-06 20:06:03 +0530

    .

---
+++ search_in_rotated_sorted_array_II.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return True
+            
+            # Case 1: duplicates
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+            
+            # Left half sorted
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            # Right half sorted
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return False
