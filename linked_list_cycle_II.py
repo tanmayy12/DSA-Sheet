5d4354186046ef70f7a81f74005f49f5f430c16c
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-04-25 17:24:07 +0530

    .

---
+++ linked_list_cycle_II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def detectCycle(self, head):
+        slow = fast = head
+        
+        # Step 1: detect cycle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                # Step 2: find cycle start
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        
+        return None
