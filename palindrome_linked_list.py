78439b67908a57603591e6a81e697cedbafa7a6b
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-06-03 17:55:59 +0530

    .

---
+++ palindrome_linked_list.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isPalindrome(self, head):
+
+        slow = fast = head
+
+        # Find middle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # Compare both halves
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+
+            left = left.next
+            right = right.next
+
+        return True
