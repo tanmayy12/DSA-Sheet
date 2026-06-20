9e08384eb6c27e1386a8b0e7cb9ded9faceefae3
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-06-20 12:06:02 +0530

    .

---
+++ more_zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0  # position for next non-zero
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
