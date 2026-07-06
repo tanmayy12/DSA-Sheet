8681e3aa5f634ffc6e5e6c8716156dfbab575703
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-07-06 11:47:53 +0530

    .

--- shuffle_an_array.py
+++ shuffle_an_array.py
@@ -1,35 +1,19 @@
-class Solution(object):
+import random
+
+class Solution:
 
     def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-
         self.original = nums[:]
 
-        
-
     def reset(self):
-        """
-        :rtype: List[int]
-        """
         return self.original[:]
 
     def shuffle(self):
-        """
-        :rtype: List[int]
-        """
         arr = self.original[:]
         n = len(arr)
 
         for i in range(n):
-            j = randint(i, n - 1)   
+            j = random.randint(i, n - 1)
             arr[i], arr[j] = arr[j], arr[i]
 
         return arr
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.reset()
-# param_2 = obj.shuffle()
