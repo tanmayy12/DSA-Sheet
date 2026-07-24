89b4c20e1a8b40f6f34ded4e782e0ddae58cfa0a
Tanmay Jagtap <tanmayjagtap12@outlook.com>
2026-07-24 15:38:55 +0530

    .

---
+++ queue_reconstruction_by_height.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+
+        queue = []
+
+        for person in people:
+            queue.insert(person[1], person)
+
+        return queue
